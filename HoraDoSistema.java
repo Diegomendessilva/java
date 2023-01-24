77b318276b38f1dc655cb48ec9d49bf34f497816
Jose Diego Mendes da silva <122491768+Diegomendessilva@users.noreply.github.com>
2023-01-24 14:55:57 -0300

    segundo programa: hora certa

---
+++ HoraDoSistema.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package estudo.horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Diego
+ */
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+     
+        Date relogio = new Date();
+        System.out.print("A hora do sistema é ");
+        System.out.print(relogio.toString());
+        
+    }
+}
